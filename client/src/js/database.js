ac71ec74942478d1c2e856ba3cbc5a1c6ecd0b9c
Raja Yaser Hussain <yaser@Yasers-MacBook-Pro.local>
2024-04-14 20:46:45 -0400

    Added code for getting and putting data to database in database.js file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,28 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try {
+    console.log('PUT to the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = jateDb.transaction('jate', 'readWrite');
+    const store = tx.objectStore('todos');
+    const request = store.put({ content });
+    const result = await request;
+    console.log('result.value', result);
+    return result;
+  } catch (err) {
+    console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+export const getDb = async () => {
+  try {
+    console.log('GET from the database');
+    const jateDb = await openDB('jate', 1);
+    const tx = 
+  }
+  console.error('getDb not implemented');
+};
 initdb();
